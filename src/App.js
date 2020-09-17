a337aa3f309a01dbdaea8e226caf62d283a15505
Alex Stout <stizout@gmail.com>
2020-09-16 22:13:53 -0700

    integrate slider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Row from './components/row';
 import './App.css';
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <main className='container'>
-        <div style={{ backgroundColor: 'orange', height: '1000px' }}>
-          <Row />
-        </div>
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'alex', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+        { id: 1, title: 'Title', img: 'img' },
+      ],
+      translateX: 0,
+    };
+  }
+
+  handleTranslate = (amount) => {
+    const { translateX } = this.state;
+    if (amount === 0) {
+      console.log('reset', amount);
+      return this.setState({ translateX: amount });
+    }
+    const translationAmount = translateX - amount;
+    console.log('state', translateX);
+    this.setState({ translateX: translationAmount });
+  };
+  render() {
+    const { translateX, books } = this.state;
+    return (
+      <div>
+        <Navbar />
+        <main className='container'>
+          <div className='row'>
+            <Row handleTranslate={this.handleTranslate} translateX={translateX} books={books} />
+          </div>
+          <div className='row'>
+            <Row />
+          </div>
+          <div className='row'>
+            <Row />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/row.js
+++ src/components/row.js
@@ -5,24 +5,41 @@ export default class row extends Component {
   constructor() {
     super();
     this.state = {
-      books: [
-        { id: 1, title: 'Title', img: 'img' },
-        { id: 1, title: 'Title', img: 'img' },
-        { id: 1, title: 'Title', img: 'img' },
-        { id: 1, title: 'Title', img: 'img' },
-        { id: 1, title: 'Title', img: 'img' },
-        { id: 1, title: 'Title', img: 'img' },
-      ],
+      count: 0,
     };
   }
+
+  determineTranslate = () => {
+    const { count } = this.state;
+    const { books, handleTranslate } = this.props;
+    console.log(count, books.length / 5);
+    if (count > books.length / 5) {
+      handleTranslate(0);
+      return this.setState({ count: 0 });
+    }
+    handleTranslate(500);
+    this.setState({ count: count + 1 });
+  };
   render() {
-    const { books } = this.state;
+    const { handleTranslate, translateX, books } = this.props;
+
+    if (books == null) {
+      return 'Loading';
+    }
+
     return (
-      <div className='flex'>
-        {books.map(() => (
-          <div className='book'>hello</div>
-        ))}
-        <div className='book'>alex</div>
+      <div>
+        <button onClick={() => this.determineTranslate()}>XY</button>
+        <div className='flex book-container'>
+          <div
+            className={`flex`}
+            style={{ transform: `translateX(${translateX}px)`, transitionDuration: '1s' }}
+          >
+            {books.map((book) => (
+              <div className='book'>{book.title}</div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
