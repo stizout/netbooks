b0a5392891287a354232a54ee1065d1b98619c4a
Alex Stout <stizout@gmail.com>
2020-09-18 22:56:50 -0700

    add real book

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Row from './components/row';
 import './App.css';
+import bookArray from './assets/books.json';
 
 const page1 = '/books/Hide-And-Seek-1.png';
 const page2 = '/books/Hide-and-Seek-2.png';
+
 class App extends Component {
   constructor() {
     super();
@@ -15,6 +17,7 @@ class App extends Component {
   }
   render() {
     const { books } = this.state;
+    console.log(bookArray);
     return (
       <Router>
         <div>
@@ -26,14 +29,14 @@ class App extends Component {
               </Route>
               <Route path='/'>
                 <div className='row'>
+                  <Row books={bookArray} />
+                </div>
+                {/* <div className='row'>
                   <Row books={books} />
                 </div>
                 <div className='row'>
                   <Row books={books} />
-                </div>
-                <div className='row'>
-                  <Row books={books} />
-                </div>
+                </div> */}
               </Route>
             </Switch>
           </main>
--- src/components/row.js
+++ src/components/row.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './css/row.css';
 
@@ -34,6 +35,12 @@ export default class row extends Component {
     this.handleTranslate(500);
     this.setState({ count: count + 1 });
   };
+
+  getBook = () => {
+    axios.get('https://www.googleapis.com/books/v1/volumes/Oaw9AQAAQBAJ').then((res) => {
+      console.log(res.data);
+    });
+  };
   render() {
     const { books } = this.props;
     const { translateX } = this.state;
@@ -41,6 +48,7 @@ export default class row extends Component {
     if (books == null) {
       return 'Loading';
     }
+    console.log(books[0]);
 
     return (
       <div>
@@ -52,13 +60,16 @@ export default class row extends Component {
           >
             XY
           </button>
+          <button onClick={() => this.getBook()}>book</button>
           <div
             className={`flex`}
             style={{ transform: `translateX(${translateX}px)`, transitionDuration: '1s' }}
           >
             {books.map((book) => (
               <Link to='/book'>
-                <div className='book'>{book.title}</div>
+                <div className='book'>
+                  <img src={book.volumeInfo.imageLinks.small} alt='book' />
+                </div>
               </Link>
             ))}
           </div>
